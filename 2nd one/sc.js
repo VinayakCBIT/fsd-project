fd8c4c812529672e6f90fdff4dd9bc722c49917e
VinayakCBIT <150412423+VinayakCBIT@users.noreply.github.com>
2023-12-05 23:15:49 +0530

    Add files via upload

---
+++ 2nd one/sc.js
@@ -0,0 +1,10 @@
+function showOptions() {
+    const optionsMenu = document.getElementById('options-menu');
+    optionsMenu.style.display = 'block';
+  }
+  
+  function hideOptions() {
+    const optionsMenu = document.getElementById('options-menu');
+    optionsMenu.style.display = 'none';
+  }
+  
---
+++ 2nd one/script.js
@@ -0,0 +1,32 @@
+function redirectToYouTube() {
+  
+  window.location.href = 'https://youtu.be/WlKU5lAJHhQ?feature=shared';
+}
+/*
+
+const fullWidthImg = document.getElementById('full-width-img');
+let currentIndex = 0;
+const totalImages = 3; // Replace with the actual number of images
+
+function showImage(index) {
+  const translateValue = -index * 1147; // Assuming each image has a width of 1147 pixels
+  fullWidthImg.style.transform = 'translateX(' + translateValue + 'px)';
+}
+
+function nextImage() {
+  currentIndex = (currentIndex + 1) % totalImages;
+  showImage(currentIndex);
+}
+
+function prevImage() {
+  currentIndex = (currentIndex - 1 + totalImages) % totalImages;
+  showImage(currentIndex);
+}
+*/
+function redirectToReddit() {
+    // Replace 'https://www.reddit.com/' with your actual Reddit link
+    window.location.href = 'https://www.reddit.com/';
+  }
+  
+
+
